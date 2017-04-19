29f8d22f03c3545183695d0e16c2df4d1151df4c
KayBrownstein <kaylivbrownstein@gmail.com>
2017-04-19 14:18:50 -0400

    create user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :country, presence: true
+
 end
