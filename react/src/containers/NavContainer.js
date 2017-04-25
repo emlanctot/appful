7d819af6e4e287a5959abb7a99365a1a260a6182
b-tramer <tramerbrad@gmail.com>
2017-04-25 16:56:41 -0400

    add search bar

--- react/src/containers/NavContainer.js
+++ react/src/containers/NavContainer.js
@@ -1,17 +1,38 @@
-import React from 'react'
+import React from 'react';
+import SearchBar from '../components/SearchBar';
 
 class NavContainer extends React.Component {
   constructor(props){
     super(props);
-    this.state = {}
+    this.state = {
+      search_term: '',
+      filtered_data: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+  }
 
+  handleSubmit(){
+    event.preventDefault();
+  }
+
+  handleSearchTermChange(){
+    this.setState({ search_term: event.target.value })
   }
 
   render() {
-
     return(
       <div>
         <h1> Navigation </h1>
+
+        <div className='searchbar'>
+          <SearchBar
+            search_term = {this.state.search_term}
+            onSubmit = {this.handleSubmit}
+            onChange = {this.handleSearchTermChange}
+          />
+        </div>
+
         {this.props.children}
       </div>
 
