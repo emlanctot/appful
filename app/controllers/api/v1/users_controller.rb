f8cf77ff99b4ae636e073b158ea65565f3910b9f
KayliBrownstein <kaylivbrownstein@gmail.com>
2017-04-25 14:02:22 -0400

    remove users api controller

--- app/controllers/api/v1/users_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::V1::UsersController < ApplicationController
-  skip_before_action :verify_authenticity_token
-  helper_method :current_user
-
-  def index
-    @user = User.find(1)
-    render json: @user
-  end
-end
