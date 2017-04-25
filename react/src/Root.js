cbc87e77689c11cb767210b897a4c55d3a8357d5
b-tramer <tramerbrad@gmail.com>
2017-04-25 10:05:58 -0400

    Add root file

---
+++ react/src/Root.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router';
+import NavContainer from './containers/NavContainer';
+import IndexContainer from './containers/IndexContainer';
+import SiteShowContainer from './containers/SiteShowContainer';
+
+class Root extends Component {
+  constructor(props){
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return(
+      <Router history={browserHistory}>
+        <Route path="/" component={NavContainer}>
+          <IndexRoute component={IndexContainer} />
+          <Route path='sites' component={SiteShowContainer} />
+          <Route path='sites/:id' component={SiteShowContainer} />
+          <Route path='sites/:id/edit' component={SiteShowContainer} />
+        </Route>
+      </Router>
+    )
+  }
+}
+
+export default Root;
