c0c262cb0eeded16b75fd5bcaf6f153270681b54
Emma Lanctot <emmalanctot@gmail.com>
2017-04-26 13:14:24 -0400

    added test coverage

--- spec/features/user_sees_reviews_spec.rb
+++ spec/features/user_sees_reviews_spec.rb
@@ -23,12 +23,15 @@ feature "User sees the reviews belonging to a site" do
     )
   end
   let(:review) do
-    Review.create(site_id: 1, user_id: 1, overall_rating: 5.0)
+    Review.create(site_id: 1, user_id: 1, overall_rating: 5.0, concept_body: 'such a good concept!', usability_body: 'it is broken!', design_body: 'it looks gross!')
   end
 
   scenario "when user visits page, sees all reviews" do
     visit `/sites/#{site.id}`
 
     expect(page).to have_content `#{review.overall_rating}`
+    expect(page).to have_content `#{review.concept_body}`
+    expect(page).to have_content `#{review.usability_body}`
+    expect(page).to have_content `#{review.design_body}`
   end
 end
