bc46020fc618b184ba2685e8fd0819791d6b356c
Emma Lanctot <emmalanctot@gmail.com>
2017-04-26 14:43:25 -0400

    fixed overflow issues with css and added font and color

--- react/src/components/SiteTile.js
+++ react/src/components/SiteTile.js
@@ -4,12 +4,14 @@ import { Link } from 'react-router';
 const SiteTile = (props) => {
   return(
     <div className="small-12 medium-6 large-4 columns site-tile">
-      <div className="sites-boxes">
-        <h3> <Link to={`/sites/${props.id}`}>{props.name}</Link> </h3>
+      <div className="box-content">
+
+        <Link to={`/sites/${props.id}`}><h3 className= 'site-title'>{props.name}</h3></Link>
         <img src="https://idagram.files.wordpress.com/2012/12/skjermbilde-2012-12-09-kl-17-38-00.png" width="400"/>
         <p> URL: <a href="${props.url}">{props.url}</a> </p>
         <p> {props.description} </p>
         <Link to='/'> <button type="button" onClick={props.handleDelete}>Delete This Site</button> </Link>
+
       </div>
     </div>
   )
