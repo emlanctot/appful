289497e22e23f2e0436be128944cfef6f4986c32
b-tramer <tramerbrad@gmail.com>
2017-04-27 17:30:44 -0400

    Fixed delete button

--- react/src/containers/SiteShowContainer.js
+++ react/src/containers/SiteShowContainer.js
@@ -34,7 +34,6 @@ class SiteShowContainer extends Component {
   componentDidMount() {
     this.getSiteData();
     this.getReviewData();
-
   }
 
   getSiteData() {
@@ -62,6 +61,7 @@ class SiteShowContainer extends Component {
   handleDelete() {
     let siteId = this.props.params.id;
     fetch(`/api/v1/sites/${siteId}`, {
+      credentials: "same-origin",
       method: "DELETE",
       headers: { "Content-Type": "application/json" }
     })
@@ -106,7 +106,6 @@ class SiteShowContainer extends Component {
   handleSubmit(event) {
     event.preventDefault();
     let reviewPayload = {
-      user_id: 1,
       overall_rating: this.state.overall_rating,
       concept_body: this.state.concept_body,
       design_body: this.state.design_body,
@@ -118,7 +117,6 @@ class SiteShowContainer extends Component {
   }
 
   sendInput(reviewPayload) {
-    console.log(reviewPayload);
     let siteId = this.props.params.id;
     fetch(`/api/v1/sites/${siteId}/reviews`, {
       credentials: "same-origin",
